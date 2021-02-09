f76fd8fed99779f25681a9c9705c27255bf6fe88
hlcphantom <chenhonglin9377@gmail.com>
2021-02-09 16:22:42 -0500

    wrap up

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'; 
@@ -10,9 +11,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Link to="/">Home</Link>
-          <Link to="/OtherPage">Other Page</Link>
-          
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -21,15 +19,15 @@ function App() {
           >
             Learn React
           </a>
-
-          <br/>
-
-          {/* add routes for navigation */}
-          <div>
-            <Route exact path="/" component={Fib}></Route>
-            <Route path="/OtherPage" component={OtherPage}></Route>
-          </div>
+          <Link to="/">Home</Link>
+          <Link to="/OtherPage">Other Page</Link>
         </header>
+
+        {/* add routes for navigation */}
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/OtherPage" component={OtherPage}/>
+        </div>
       </div>
     </Router>
   );
